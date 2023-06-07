52ad09ddff270672ec81c9f44d00e96a9c159191
Aadi Nashikkar <aadinashikkar@Aadis-MacBook-Pro-2.local>
2023-06-07 01:39:21 -0700

    First commit

---
+++ Processor.py
@@ -0,0 +1,60 @@
+# Tasks from basic: add splits, print actual smiles, add latent space knowledge
+
+import selfies
+from sklearn.model_selection import train_test_split
+import pickle
+import torch
+
+x_values = []
+
+with open('CS197_dataset/DATASET.csv', 'r') as f:
+    lines = f.readlines()
+
+with open('CS197_dataset/ESM_embedding.pickle', 'rb') as fd:
+    data = pickle.load(fd)
+
+# overrides default constraints
+selfies.set_semantic_constraints({'?': 8})
+
+selfies_list = []
+
+for item in lines:
+    A = item.split(',')
+    encoded = selfies.encoder(A[0])
+    selfies_list.append(encoded)
+
+all_selfies_symbols = selfies.get_alphabet_from_selfies(selfies_list)
+selfies_alphabet = list(all_selfies_symbols)
+largest_selfies_len = max(selfies.len_selfies(s) for s in selfies_list)
+
+### borrowed
+pad_token = ''
+char_to_idx = {x:i for i,x in enumerate(selfies_alphabet)}
+char_to_idx[pad_token] = len(char_to_idx)
+###
+
+print(char_to_idx)
+
+for i, item in enumerate(selfies_list):
+    ### borrowed
+    selfies_chr = item.split('[')
+    selfies_chr = ['['+x for x in selfies_chr if x != '']
+    encoded = [char_to_idx[char.rstrip('.')] for char in selfies_chr]
+    encoded += [char_to_idx[pad_token]] * (largest_selfies_len - len(encoded))
+    encoded = [x/len(char_to_idx) for x in encoded] # normalize to between 0-1
+    encoded_tensor = torch.tensor(encoded, dtype=torch.float32)
+    ###
+
+    # We formerly appended tuples. Now, we will just append a concatenation
+    concat = torch.cat((encoded_tensor, torch.sigmoid(data[i])))
+    x_values.append(concat)
+
+combined_repr_tensor = torch.stack(x_values)
+print(combined_repr_tensor)
+
+# Save the tensor
+torch.save(combined_repr_tensor, './Data/combined_repr_tensor.pt')
+
+# Save the char_to_idx mapping
+with open('./Data/char_to_idx.pickle', 'wb') as handle:
+    pickle.dump(char_to_idx, handle)
---
+++ VAE.py
@@ -0,0 +1,95 @@
+import torch
+from torch import nn
+from torch.optim import Adam
+from torch.utils.data import DataLoader, TensorDataset
+import torch.nn.functional as F
+
+class VAE(nn.Module):
+    def __init__(self, in_dimension, layer_1d, layer_2d, layer_3d, latent_dimension):
+        super(VAE, self).__init__()
+        
+        self.encoder = nn.Sequential(
+            nn.Linear(in_dimension, layer_1d),
+            nn.ReLU(),
+            nn.Linear(layer_1d, layer_2d),
+            nn.ReLU(),
+            nn.Linear(layer_2d, layer_3d),
+            nn.ReLU(),
+            nn.Linear(layer_3d, latent_dimension*2)
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dimension, layer_3d),
+            nn.ReLU(),
+            nn.Linear(layer_3d, in_dimension),
+            nn.Sigmoid()
+        )
+
+    def reparameterize(self, mu, logvar):
+        # Reparameterization trick
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+        
+    ### borrowed
+    def forward(self, x):
+        # Encoding
+        h = self.encoder(x)
+        mu, logvar = torch.chunk(h, 2, dim=-1)
+
+        # Reparameterization
+        z = self.reparameterize(mu, logvar/4)
+
+        # Decoding
+        x_recon = self.decoder(z)
+
+        return x_recon, mu, logvar
+    ###
+
+# Check if GPU is available and if not, fall back to CPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f'Using device: {device}')
+
+num_epochs = 10
+
+# Load the data
+data = torch.load('./Data/combined_repr_tensor.pt')
+dataset = TensorDataset(data)
+batch_size = 32  # adjust the batch size as needed
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+# in_dimension, layer_1d, layer_2d, layer_3d, latent_dimension
+model = VAE(in_dimension=2516, layer_1d=1500, layer_2d=1000, layer_3d=250, latent_dimension=50).to(device)
+optimizer = Adam(model.parameters(), lr=1e-3)
+
+def loss_function(x_decoded, x, mu, logvar):
+    xent_loss = F.mse_loss(x_decoded, x)
+    kl_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    return (0.1 * kl_loss) + xent_loss
+
+# Training loop
+for epoch in range(num_epochs):
+    ### borrowed
+    total_loss = 0.0
+    num_batches = 0
+    
+    for batch in dataloader:
+        optimizer.zero_grad()
+
+        batch = batch[0].to(device)  # move batch data to the device
+        recon_batch, mu, logvar = model(batch)
+        loss = loss_function(recon_batch, batch, mu, logvar)
+        total_loss += loss.item()
+        num_batches += 1
+        loss.backward()
+        optimizer.step()
+
+    avg_loss = total_loss / num_batches
+    print(f'Epoch {epoch+1}, Loss: {avg_loss}')
+    
+    single_batch_example = batch[0][0: 1236] # Remember that the first 1236 elements correspons to the small molecule
+    recon_example        = recon_batch[0][0: 1236]
+    print('Label molecul: ', single_batch_example * 111) # Remeber that 111 is the number of unique selfies characters we have 
+    print('Recon example: ', recon_example* 111)
+    ### end borrowed
+
