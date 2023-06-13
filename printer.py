ffa4167ff5deccc99d9af9c3c932796eda019101
aliyanishfaq <aliyan@stanford.edu>
2023-06-13 14:37:27 -0700

    modified:   __pycache__/printer.cpython-310.pyc 	modified:   printer.py

--- printer.py
+++ printer.py
@@ -6,12 +6,12 @@ with open('./Data/char_to_idx.pickle', 'rb') as handle:
 
 def intToSelfies(encoded):
     encoded = encoded.tolist()
-    encoded_rounded = [int(round(i)) if i < 110.5 else 110 for i in encoded]
+    encoded_rounded = [int(round(i)) if i < 105.5 else 105 for i in encoded]
 
     num_elems = len(encoded_rounded)
-    for j in range(1, num_elems - 1):  # to remove redundant values at the end
-        if encoded_rounded[j - 1] == encoded_rounded[j] == encoded_rounded[j + 1]:
-            encoded_rounded = encoded_rounded[:j - 1]
+    for j in range(2, num_elems - 2):  # to remove redundant values at the end
+        if encoded_rounded[j - 2] == encoded_rounded[j - 1] == encoded_rounded[j] == encoded_rounded[j + 1] == encoded_rounded[j + 2]:
+            encoded_rounded = encoded_rounded[:j - 2]
             break
 
     idx_to_char = {idx: char for char, idx in char_to_idx.items()}
